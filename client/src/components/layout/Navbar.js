d542cb8eddc2ab15a5b437a9cd4f2e2bdf96b68c
khunshan97 <khunshan.butt@gmail.com>
2020-03-11 17:07:55 +0500

    Icons in NavBar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,21 +8,27 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <Link to='/profiles'>Profiles</Link>
-      </li>
-      <li>
-        <Link to='/posts'>Posts</Link>
-      </li>
-      <li>
-        <Link to='/dashboard'>
-          <i className='fas fa-user' />{" "}
-          <span className='hide-sm'>Dashboard</span>
+        <Link to="/profiles">
+          <i className="fas fa-users" />{" "}
+          <span className="hide-sm">Profiles</span>
         </Link>
       </li>
       <li>
-        <a onClick={logout} href='/'>
-          <i className='fas fa-sign-out-alt' />{" "}
-          <span className='hide-sm'>Logout</span>
+        <Link to="/posts">
+          <i className="fas fa-list-alt" />{" "}
+          <span className="hide-sm">Posts</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href="/">
+          <i className="fas fa-sign-out-alt" />{" "}
+          <span className="hide-sm">Logout</span>
         </a>
       </li>
     </ul>
@@ -31,22 +37,22 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to='/profiles'>Profiles</Link>
+        <Link to="/profiles">Profiles</Link>
       </li>
       <li>
-        <Link to='/register'>Register</Link>
+        <Link to="/register">Register</Link>
       </li>
       <li>
-        <Link to='/login'>Login</Link>
+        <Link to="/login">Login</Link>
       </li>
     </ul>
   );
 
   return (
-    <nav className='navbar bg-dark'>
+    <nav className="navbar bg-dark">
       <h1>
-        <Link to='/'>
-          <i className='fas fa-code'></i> Konnect
+        <Link to="/">
+          <i className="fas fa-handshake"></i> Altruist
         </Link>
       </h1>
       {!loading && (
